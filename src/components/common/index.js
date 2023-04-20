588701e0e4a5bb94f0a2ee9bf593ed3962ffc66c
bin000527 <bin000527@sookmyung.ac.kr>
2023-04-20 14:33:43 +0900

    add: common 컴포넌트 index.js 추가

---
+++ src/components/common/index.js
@@ -0,0 +1,9 @@
+export { default as Button } from './Button';
+export { default as CategoryTag } from './CategoryTag';
+export { default as Header } from './Header';
+export { default as Modal } from './Modal';
+export { default as SideBanner } from './SideBanner';
+export { default as StoreItem } from './StoreItem';
+export { default as StoreItemOnHover } from './StoreItemOnHover';
+
+export { default as Responsive } from './Responsive';
