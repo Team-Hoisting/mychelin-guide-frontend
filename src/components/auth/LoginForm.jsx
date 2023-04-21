3444e6cf958bcb29a298ce843ecf014c82926632
Gyeongm1n <affirming_km@naver.com>
2023-04-21 17:11:48 +0900

    Refactor: api 요청 방식 변경

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useMutation } from '@tanstack/react-query';
 import { useSetRecoilState } from 'recoil';
 import userState from '../../recoil/atoms/userState';
-import queryKey from '../../constants/userQueryKey';
 import { signIn } from '../../api/auth';
 import { signinSchema } from '../../schema';
 import AuthForm from './AuthForm';
@@ -18,21 +16,23 @@ const LoginForm = () => {
   const navigate = useNavigate();
   const setUser = useSetRecoilState(userState);
   const [isFailedLogin, setIsFailedLogin] = React.useState(false);
-  const { mutate } = useMutation({
-    queryKey,
-    mutationFn: signIn,
-    onSuccess(data) {
+
+  const onSubmit = async data => {
+    try {
+      const user = await signIn(data);
+
+      console.log(user);
+
       setUser(data);
       navigate('/');
-    },
-    onError() {
+    } catch (e) {
       setIsFailedLogin(true);
-    },
-  });
+    }
+  };
 
   return (
     <>
-      <AuthForm type="login" formSchema={signinSchema} defaultValues={defaultValues} request={mutate} />
+      <AuthForm type="login" formSchema={signinSchema} defaultValues={defaultValues} request={onSubmit} />
       {isFailedLogin && <LoginFailAlert close={() => setIsFailedLogin(false)} />}
     </>
   );
--- src/components/auth/RegisterForm.jsx
+++ src/components/auth/RegisterForm.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useMutation } from '@tanstack/react-query';
 import { useSetRecoilState } from 'recoil';
 import userState from '../../recoil/atoms/userState';
-import queryKey from '../../constants/userQueryKey';
 import { signUp } from '../../api/auth';
 import { signupSchema } from '../../schema';
 import AuthForm from './AuthForm';
@@ -18,16 +16,21 @@ const defaultValues = {
 const RegisterForm = () => {
   const navigate = useNavigate();
   const setUser = useSetRecoilState(userState);
-  const { mutate } = useMutation({
-    queryKey,
-    mutationFn: signUp,
-    onSuccess(data) {
+
+  const onSubmit = async data => {
+    try {
+      const user = await signUp(data);
+
+      console.log(user);
+
       setUser(data);
       navigate('/');
-    },
-  });
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-  return <AuthForm type="register" formSchema={signupSchema} defaultValues={defaultValues} request={mutate} />;
+  return <AuthForm type="register" formSchema={signupSchema} defaultValues={defaultValues} request={onSubmit} />;
 };
 
 export default RegisterForm;
