4d30e563b9042a3a900a3df44878b12546872972
minji99710 <alswl99710@gmail.com>
2023-04-23 16:48:07 +0900

    Style: 모달 중앙 고정

--- src/components/modal/Vote.jsx
+++ src/components/modal/Vote.jsx
@@ -7,6 +7,13 @@ import categoryInfo from '../../constants/categoryInfo';
 
 const Container = styled.div`
   padding: 2rem 1.2rem;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: #fff;
+  border-radius: 8px;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;
 `;
 
 const Info = styled.div`
