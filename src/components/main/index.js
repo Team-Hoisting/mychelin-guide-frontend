e7ab5991fc272261ac0bcc78e2522b2673127b09
Kyu Sung Sohn <rok.ksohn@gmail.com>
2023-04-28 19:59:26 +0900

    Refactor: 메인페이지, InfiniteStoreList 컴포넌트 분리

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,3 +1,4 @@
 export { default as Categories } from './Categories';
 export { default as StoreItemOnHover } from './StoreItemOnHover';
 export { default as NoResultMessage } from './NoResultMessage';
+export { default as InfiniteStoreList } from './InfiniteStoreList';
