7e4a793b3c2cc031f198fe18901fe1fadeb701fa
Gyeongm1n <affirming_km@naver.com>
2023-05-01 22:12:06 +0900

    Fix: 오류수정

--- src/components/searchmap/ResultItemOnHover.jsx
+++ src/components/searchmap/ResultItemOnHover.jsx
@@ -1,9 +1,7 @@
-import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import { Link } from 'react-router-dom';
 import { Button } from '../common';
-import { fetchStore } from '../../api/stores';
 import NewModal from '../common/NewModal';
 
 const Container = styled.main`
@@ -40,18 +38,6 @@ const InfoText = styled.p`
 `;
 
 const ResultItemOnHover = ({ storeId, isRegistered, storeName, address, phoneNumber, x, y }) => (
-  // useEffect(() => {
-  //   (async () => {
-  //     try {
-  //       const storeInfo = await fetchStore(storeId)();
-
-  //       setIsRegistered(!!storeInfo);
-  //     } catch (e) {
-  //       console.log(e);
-  //     }
-  //   })();
-  // }, []);
-
   <Container>
     <ButtonContainer>
       {isRegistered ? (
