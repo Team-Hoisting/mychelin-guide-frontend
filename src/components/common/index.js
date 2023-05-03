18427bd7674023173ec8d31753547f267c19e371
bin000527 <bin000527@sookmyung.ac.kr>
2023-05-03 15:32:20 +0900

    refactor: 사용하지 않는 Modal.jsx 삭제

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,7 +1,6 @@
 export { default as Button } from './Button';
 export { default as CategoryTag } from './CategoryTag';
 export { default as Header } from './Header';
-export { default as Modal } from './Modal';
 export { default as SideBanner } from './SideBanner';
 export { default as LogInBanner } from './LogInBanner';
 export { default as CategoryBox } from './CategoryBox';
