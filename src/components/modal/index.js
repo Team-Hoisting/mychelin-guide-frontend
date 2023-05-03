6962424a5832f779b3767b6f2716ea48551080a6
bin000527 <bin000527@sookmyung.ac.kr>
2023-05-03 16:07:05 +0900

    refactor: 사용하지 않는 컴포넌트 제거 및 modal index.js 정리

---
+++ src/components/modal/index.js
@@ -0,0 +1,8 @@
+export { default as ButtonGroup } from './ButtonGroup';
+export { default as CategorySelector } from './CategorySelector';
+export { default as DuplicateStore } from './DuplicateStore';
+export { default as SameCategoryChecker } from './SameCategoryChecker';
+export { default as SameStoreChecker } from './SameStoreChecker';
+export { default as Success } from './Success';
+export { default as SuccessVerifier } from './SuccessVerifier';
+export { default as Vote } from './Vote';
