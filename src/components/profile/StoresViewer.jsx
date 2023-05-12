ba7f9de258a8a77c9e6d96afed77450d1afe167b
minji99710 <alswl99710@gmail.com>
2023-05-12 19:21:58 +0900

    Style: 다크모드 수정

--- src/components/profile/StoresViewer.jsx
+++ src/components/profile/StoresViewer.jsx
@@ -15,7 +15,7 @@ const Tabs = styled.div`
 
 const Tab = styled.div`
   width: 250px;
-  border-top: ${({ selected }) => (selected ? '2px solid var(--border-top)' : '2px solid rgba(0,0,0,0)')};
+  border-top: ${({ selected }) => (selected ? '2px solid var(--border-tertiary)' : '2px solid rgba(0,0,0,0)')};
   color: ${({ selected }) => (selected ? 'var(--font-color)' : '#ababab')};
   font-size: 20px;
   font-weight: 600;
